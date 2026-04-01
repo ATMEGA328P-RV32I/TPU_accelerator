a77f159efff27e5d79da638d82588e9994e89c4e
ATMEGA328P-RV32I <itsatul1831@gmail.com>
2026-04-01 21:28:47 +0530

    Update tpu_pool_engine.sv

--- svfiles/tpu_pool_engine.sv
+++ svfiles/tpu_pool_engine.sv
@@ -1,4 +1,18 @@
-// Copyright 2026 Atul Kumar
+// Custom Tensor Processing Unit (TPU) for 1D CNN Inference
+// Copyright (C) 2026 Atul Kumar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module tpu_pool_engine(
     input logic clk,rst_n,start, // start is the trigger to wake up the engine
     output logic engine_done, // flag raised to tell that pooling is done
