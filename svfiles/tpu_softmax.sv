41f4fd28d46115cbaf545e061fea85ff706fb577
ATMEGA328P-RV32I <itsatul1831@gmail.com>
2026-04-01 21:29:14 +0530

    Update tpu_softmax.sv

--- svfiles/tpu_softmax.sv
+++ svfiles/tpu_softmax.sv
@@ -1,6 +1,18 @@
-// Copyright 2026 Atul Kumar
-// high precision hardware softmax 
-// Idea- Use memory to reduce dependcece on DSP blocks
+// Custom Tensor Processing Unit (TPU) for 1D CNN Inference
+// Copyright (C) 2026 Atul Kumar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module tpu_softmax(
     input logic signed [31:0] logit_normal,
     input logic signed [31:0] logit_anomaly,
