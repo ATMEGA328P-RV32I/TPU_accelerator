3eaf9f6ae14806c891ea4001abd59ac20afc396d
ATMEGA328P-RV32I <itsatul1831@gmail.com>
2026-04-01 21:33:59 +0530

    Update mac_node.sv

--- svfiles/mac_node.sv
+++ svfiles/mac_node.sv
@@ -1,7 +1,18 @@
-// Copyright 2026 Atul Kumar
-// Module: mac_node
-// Description: The fundamental Processing Element (Multiply-Accumulate)
-// Note: I have made this a weight-stationary architecture.
+// Custom Tensor Processing Unit (TPU) for 1D CNN Inference
+// Copyright (C) 2026 Atul Kumar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module mac_node #( parameter int D_WIDTH=16)(
     input logic clk,
     input logic rst_n,
